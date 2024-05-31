0a9edb94c92726e535c5a04dfdbd3d9062457e20
gaelLara21 <gaellara19@gmail.com>
2024-05-31 13:34:21 -0600

    06 Implements Graphics

--- pollsapp/polls/views.py
+++ pollsapp/polls/views.py
@@ -1,19 +1,13 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render, redirect
-
 from .models import Question, Choice
 
-# Create your views here.
 def home(request):
     questions = Question.objects.all()
-    #return HttpResponse("Welcome Home")
-    return render(request, 'polls/home.html', {
-        "questions": questions
-    })
+    return render(request, 'polls/home.html', {"questions": questions})
 
 def vote(request, q_id):
     question = get_object_or_404(Question, id=q_id)
-
     if request.method == 'POST':
         try:
             choice_id = request.POST['choice']
@@ -26,12 +20,20 @@ def vote(request, q_id):
                 "question": question,
                 "error_message": "Debes seleccionar una opción válida."
             })
-    return render(request, 'polls/question.html',{"question": question})
+    return render(request, 'polls/question.html', {"question": question})
 
 def results(request, q_id):
     try:
-       question = Question.objects.get(id=q_id)
+        question = Question.objects.get(id=q_id)
     except Question.DoesNotExist:
         raise Http404("Question Does Not Exist")
-    return render(request, 'polls/results.html', 
-                  {"question": question})
+    
+    choices = question.choice_set.all()
+    labels = [choice.choice_text for choice in choices]
+    data = [choice.votes for choice in choices]
+
+    return render(request, 'polls/results.html', {
+        "question": question,
+        "labels": labels,
+        "data": data,
+    })
